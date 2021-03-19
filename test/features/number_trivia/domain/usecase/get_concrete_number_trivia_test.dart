add4eb2e80d24a1863b7898880a3472e106ba626
rashid <mohammed1rashid786@gmail.com>
2021-03-19 14:47:21 +0530

    test

--- test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,5 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
@@ -9,9 +11,20 @@ class MockNumberTriviaRepository extends Mock
 void main() {
   GetConcreteNumberTrivia useCase;
   MockNumberTriviaRepository mockNumberTriviaRepository;
-
+  final testNumber = 1;
+  final testNumberTrivia = NumberTrivia(number: 1, text: "test");
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     useCase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+  test("should get trivia for the number from the repository", () async {
+    //arrange
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any)).thenAnswer(
+      (realInvocation) async => Right(testNumberTrivia),
+    );
+
+    //act
+
+    //assert
   });
 }
